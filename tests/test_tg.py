2fb1f7ce93b4a7adba7dec847116b1e5120deb4b
Tohru <65994850+Tohrusky@users.noreply.github.com>
2024-12-24 13:17:31 +0000

    build: update docker compose deploy (#27)

--- tests/test_tg.py
+++ tests/test_tg.py
@@ -8,8 +8,6 @@ from animepipeline.template import get_telegram_text
 
 from .util import CONFIG_PATH
 
-video_key = "test_144p.mp4"
-
 
 @pytest.mark.skipif(os.environ.get("GITHUB_ACTIONS") == "true", reason="Only test locally")
 async def test_tg_bot() -> None:
@@ -19,9 +17,9 @@ async def test_tg_bot() -> None:
 
     await sender.send_text(
         text=get_telegram_text(
-            chinese_name="败犬女主太多了！",
+            chinese_name="from unit test ~~~  |  败犬女主太多了！",
             episode=2,
             file_name="[TensoRaws] Make Heroine ga Oosugiru! [02] [1080p AVC-8bit FLAC].mkv",
-            torrent_file_hash="700a6ed3967740eb02f12a3b346696b1170b20f8",
+            torrent_file_hash="this_is_a_fake_hash",
         )
     )
